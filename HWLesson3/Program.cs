299690652870cdc1bc6ba453ad98916c81c37f31
Yuriy Slobodyanik <uslobodyanik@gmail.com>
2021-11-14 18:35:14 +0200

    Home Work, Lesson 3, Task 1-7 Done

--- HWLesson3/Program.cs
+++ HWLesson3/Program.cs
@@ -7,15 +7,16 @@ namespace HWLesson3
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine($"Check for Task 1, Power A to B {LibraryForHWLesson3.Power(3,3)}");
-            //Console.WriteLine($"Check for Task 2, Divider for 5 = {LibraryForHWLesson3.Divider(5, 10)}");
-            //Console.WriteLine($"Check for Task 3, Number of positive integers whose square is less than 20 = {LibraryForHWLesson3.InputNumberInSquereByA(20,3)}");
-            //Console.WriteLine($"Check for Task 4, Max divider for 20 = {LibraryForHWLesson3.MaxDividerByA(20)}");
-            //Console.WriteLine($"Check for Task 5, Sum of numbers from 2 to 5 = " +
-            //    $"{LibraryForHWLesson3.SumNumberForRengeAtoB(2,5)}");
-            //Console.WriteLine($"Check for Task 6, Range Fibonachi = {LibraryForHWLesson3.Fibonachi(1)}");
-            //Console.WriteLine($"Check for Task 6, Common Divisor for 3 and 9" +
-            //    $" {LibraryForHWLesson3.CommonDivisorForAbyB(8,26)}");
+            Console.WriteLine($"Check for Task 1, Power A to B {LibraryForHWLesson3.Power(3, 3)}");
+            Console.WriteLine($"Check for Task 2, Divider for 5 = {LibraryForHWLesson3.Divider(5, 10)}");
+            Console.WriteLine($"Check for Task 3, Number of positive integers whose square is less than 20 = {LibraryForHWLesson3.InputNumberInSquereByA(20, 3)}");
+            Console.WriteLine($"Check for Task 4, Max divider for 20 = {LibraryForHWLesson3.MaxDividerByA(20)}");
+            Console.WriteLine($"Check for Task 5, Sum of numbers from 2 to 5 = " +
+                $"{LibraryForHWLesson3.SumNumberForRengeAtoB(2, 5)}");
+            Console.WriteLine($"Check for Task 6, Range Fibonachi = {LibraryForHWLesson3.Fibonachi(1)}");
+            Console.WriteLine($"Check for Task 6, Common Divisor for 3 and 9" +
+                $" {LibraryForHWLesson3.CommonDivisorForAbyB(8, 26)}");
+
         }
     }
 }
